2a06cd65ca5bc1a45769c11f1a045d24f3c67fdb
Will Drougas <wdrougas@gmail.com>
2019-09-21 14:41:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,7 +2,8 @@
 def hopper
 	programmer_hash = 
  		{
-        
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
         
         :grace_hopper => {
           :known_for => "COBOL",
