ba1f17ce6528c34ab617e09b0b4e5eeb0b432803
Will Drougas <wdrougas@gmail.com>
2019-09-21 14:52:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -38,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
