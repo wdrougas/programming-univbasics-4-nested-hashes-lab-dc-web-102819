4569056e1557f9bd9f15c0f283c39b908d8b05c5
Will Drougas <wdrougas@gmail.com>
2019-09-21 15:02:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     
-  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = 
      
 end
 
