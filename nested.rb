44136c6ea169d0af149525dce6b6819279ce6142
Will Drougas <wdrougas@gmail.com>
2019-09-21 14:57:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-  programmer_hash[:yukihiro_matsumoto][{:known_for => "Ruby",}][{:languages => ["LISP", "C"]}]
-  puts programmer_hash
 end
 
 def changing_alan
